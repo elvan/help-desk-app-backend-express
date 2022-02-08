2727e21ac573a6389cbe375fe2b955c187e44c48
Elvan H <elvanhd@gmail.com>
2022-02-08 16:04:25 +0700

    registering user

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,8 +1,10 @@
 const asyncHandler = require('express-async-handler');
+const bcrypt = require('bcryptjs');
+const User = require('../models/userModel');
 
 // @route /api/users/register
 // @access Public
-exports.registerUser = asyncHandler((req, res) => {
+exports.registerUser = asyncHandler(async (req, res) => {
   const { name, email, password } = req.body;
 
   // Simple validation
@@ -11,9 +13,36 @@ exports.registerUser = asyncHandler((req, res) => {
     throw new Error('Please provide all required fields');
   }
 
-  res.status(200).json({
-    message: 'Register route is working',
+  // Check for existing user
+  const user = await User.findOne({ email: email });
+  if (user) {
+    res.status(400);
+    throw new Error('User already exists');
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  // Create user
+  const newUser = await User.create({
+    name: name,
+    email: email,
+    password: hashedPassword,
   });
+
+  if (newUser) {
+    res.status(200).json({
+      message: 'Register route is working',
+      user: {
+        id: newUser._id,
+        name: newUser.name,
+        email: newUser.email,
+      },
+    });
+  }
+
+  // If user is not created
+  throw new Error('User not created');
 });
 
 // @route /api/users/login
