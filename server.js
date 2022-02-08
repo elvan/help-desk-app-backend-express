b0902c34fd873bdcd1a97857a5018b0f95c4c2d7
Elvan H <elvanhd@gmail.com>
2022-02-08 14:56:24 +0700

    basic express server setup

--- server.js
+++ server.js
@@ -1,1 +1,21 @@
-console.log('Server is running');
+const dotenv = require('dotenv');
+const express = require('express');
+
+dotenv.config();
+
+const PORT = process.env.PORT || 5000;
+const NODE_ENV = process.env.NODE_ENV || 'development';
+const MONGODB_SERVER =
+  process.env.MONGODB_SERVER || 'mongodb://localhost:27017';
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.status(200).json({
+    message: 'Welcome to the Help Desk server',
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT} in ${NODE_ENV} mode`);
+});
